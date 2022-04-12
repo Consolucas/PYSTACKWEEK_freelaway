356873063d63e4edb287f7fefcb2bf3fe425d1e1
Lucas Consolo <lucas.consolo@gmail.com>
2022-04-11 22:10:14 -0300

    updating empty url

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', views.cadastro, name='cadastro'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('logar/', views.logar, name='logar'),
     path('sair/', views.sair, name='sair'),
--- freelaway/urls.py
+++ freelaway/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', include ('autenticacao.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('autenticacao.urls')),
     path('jobs/', include('jobs.urls'))
